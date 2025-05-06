1bc6f71ad66681742548ddc22504ed78845794d8
yanagiyy <210198714+yanagiyy@users.noreply.github.com>
2025-05-06 22:06:41 +0900

    プロジェクト ファイルを追加します。

---
+++ WpfCalculator2025/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfCalculator2025
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
