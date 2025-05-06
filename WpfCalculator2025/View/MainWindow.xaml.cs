0414eaf898cfb59feac407f652170506ccfeb5a1
yanagiyy <210198714+yanagiyy@users.noreply.github.com>
2025-05-06 22:14:40 +0900

    Model/View/ViewModelディレクトリの作成

--- WpfCalculator2025/View/MainWindow.xaml.cs
+++ WpfCalculator2025/View/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfCalculator2025
+namespace WpfCalculator2025.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
