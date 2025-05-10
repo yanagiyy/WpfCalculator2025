d193e9174c24180eda40968328dda7314aacfce4
yanagiyy <210198714+yanagiyy@users.noreply.github.com>
2025-05-10 13:29:19 +0900

    ViewModelを実装 とりあえずボタン入力をディスプレイに表示できるようにした RelayCommandはライブラリを利用（自作にするかは余裕があれば） CommunityToolkit.Mvvm

---
+++ WpfCalculator2025/ViewModel/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace WpfCalculator2025.ViewModel
+{
+    internal partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string displayText = "0";
+
+        [RelayCommand]
+        private void KeyInput(string value)
+        {
+            // とりあえず入力コマンドを表示できるように
+            DisplayText = value;
+        }
+    }
+}
