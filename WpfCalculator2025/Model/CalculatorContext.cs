baac9967f4fb72a05a896d099ae56a056160da94
yanagiyy <210198714+yanagiyy@users.noreply.github.com>
2025-05-10 14:11:29 +0900

    電卓コンテキストクラスを追加

---
+++ WpfCalculator2025/Model/CalculatorContext.cs
@@ -0,0 +1,25 @@
+﻿namespace WpfCalculator2025.Model
+{
+    /// <summary>
+    /// 電卓の状態保持制御クラス
+    /// </summary>
+    public class CalculatorContext
+    {
+        /// <summary>
+        /// 現在入力中の値
+        /// </summary>
+        private string _currentInput = string.Empty;
+
+        /// <summary>
+        /// 表示用文字列
+        /// </summary>
+        /// <returns></returns>
+        public string DisplayText
+        {
+            get
+            {
+                return string.IsNullOrEmpty(_currentInput) ? "0" : _currentInput;
+            }
+        }
+    }
+}
