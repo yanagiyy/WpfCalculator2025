cb0174e50ea144f2870fd82b6470dca93e50a275
yanagiyy <210198714+yanagiyy@users.noreply.github.com>
2025-05-10 19:00:15 +0900

    GUIと計算機能を接続

--- WpfCalculator2025/ViewModel/MainViewModel.cs
+++ WpfCalculator2025/ViewModel/MainViewModel.cs
@@ -1,18 +1,52 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using WpfCalculator2025.Model;
 
 namespace WpfCalculator2025.ViewModel
 {
     internal partial class MainViewModel : ObservableObject
     {
+
+        private CalculatorContext _calculatorContext = new CalculatorContext();
+
+        public MainViewModel()
+        {
+            displayText = _calculatorContext.DisplayText;
+        }
+
         [ObservableProperty]
-        private string displayText = "0";
+        private string displayText;
 
         [RelayCommand]
-        private void KeyInput(string value)
+        private void KeyInput(string key)
         {
-            // とりあえず入力コマンドを表示できるように
-            DisplayText = value;
+            // 数字キー or ドット
+            if (char.IsDigit(key, 0) || key == ".")
+            {
+                _calculatorContext.AddDigit(key);
+            }
+            // 演算子キー (+ - * /)
+            else if (key == "+" || key == "-" || key == "*" || key == "/")
+            {
+                _calculatorContext.SetOperator(key);
+            }
+            // イコールキー
+            else if (key == "=")
+            {
+                _calculatorContext.Compute();
+            }
+            // 全クリア（C）
+            else if (key == "C")
+            {
+                _calculatorContext.ClearAll();
+            }
+            // それ以外
+            else
+            {
+                // nop
+            }
+
+            DisplayText = _calculatorContext.DisplayText;
         }
     }
 }
