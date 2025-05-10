b8689b964ece87fc6d02e6d6288dafd126b18c45
yanagiyy <210198714+yanagiyy@users.noreply.github.com>
2025-05-10 17:49:38 +0900

    電卓機能の初期実装

--- WpfCalculator2025/Model/CalculatorContext.cs
+++ WpfCalculator2025/Model/CalculatorContext.cs
@@ -5,10 +5,20 @@
     /// </summary>
     public class CalculatorContext
     {
-        /// <summary>
-        /// 現在入力中の値
-        /// </summary>
-        private string _currentInput = string.Empty;
+        private const string InitialCurrentInput = "";
+        private const string InitialDisplayTex = "0";
+        private const string InitialOperator = "";
+        private const string InitialPreviousInput = "";
+        private const string InitialLastInput = "";
+        private const string InitialLastResult = "";
+
+        private string _currentInput = InitialCurrentInput;
+        private string _operator = InitialOperator;
+        private string _previousInput = InitialPreviousInput;
+        private string _displayText = InitialDisplayTex;
+
+        private string _lastInput = InitialLastInput;
+        private string _lastResult = InitialLastResult;
 
         /// <summary>
         /// 表示用文字列
@@ -18,8 +28,134 @@
         {
             get
             {
-                return string.IsNullOrEmpty(_currentInput) ? "0" : _currentInput;
+                return string.IsNullOrEmpty(_currentInput) ? InitialDisplayTex : _displayText;
             }
+        }
+
+        /// <summary>
+        /// 演算子
+        /// </summary>
+        public string Operator => _operator;
+
+        /// <summary>
+        /// 被オペラント
+        /// </summary>
+        public string PreviousInput => _previousInput;
+
+        /// <summary>
+        /// 演算実行直後判定
+        /// </summary>
+        /// <returns></returns>
+        private bool isComputeJustEnterd => _lastInput == "=";
+
+        /// <summary>
+        /// 演算子入力直後判定
+        /// </summary>
+        /// <returns></returns>
+        private bool isOperatorJustEnterd => "+-*/".IndexOf(_lastInput) >= 0;
+
+        /// <summary>
+        /// 計算可能状態判定
+        /// </summary>
+        /// <returns></returns>
+        private bool canConpute =>
+         !string.IsNullOrEmpty(_previousInput)
+                && !string.IsNullOrEmpty(_operator)
+                && !isComputeJustEnterd
+                && !isOperatorJustEnterd;
+
+        /// <summary>
+        /// 入力桁追加
+        /// </summary>
+        /// <param name="input"></param>
+        public void AddDigit(string input)
+        {
+
+            if (string.IsNullOrEmpty(_currentInput) || isOperatorJustEnterd)
+            {
+                _currentInput = input;
+            }
+            else
+            {
+                _currentInput += input;
+            }
+
+            _displayText = _currentInput;
+            _lastInput = input;
+        }
+
+        /// <summary>
+        /// 全クリア
+        /// </summary>
+        public void ClearAll()
+        {
+            _currentInput = InitialCurrentInput;
+            _operator = InitialOperator;
+            _previousInput = InitialPreviousInput;
+            _lastInput = InitialLastInput;
+            _lastResult = InitialLastResult;
+        }
+
+        /// <summary>
+        /// 演算実行
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        public void Compute(string inputOperator = "=")
+        {
+            if (isComputeJustEnterd)
+            {
+                // 連続演算の場合は直前の結果に対して同じ計算を繰り返す
+                _previousInput = _lastResult;
+            }
+
+            switch (_operator)
+            {
+                case "+":
+                    var previousValue = decimal.Parse(_previousInput);
+                    var currentValue = decimal.Parse(_currentInput);
+
+                    var resut = previousValue + currentValue;
+
+                    _displayText = resut.ToString();
+                    _lastResult = resut.ToString();
+                    break;
+                default:
+                    break;
+            }
+
+            _lastInput = inputOperator;
+        }
+
+        /// <summary>
+        /// 演算子設定
+        /// </summary>
+        /// <param name="inputOperator">演算子文字列</param>
+        public void SetOperator(string inputOperator)
+        {
+            if (canConpute)
+            {
+                Compute(inputOperator);
+            }
+
+            if (_currentInput == InitialCurrentInput)
+            {
+                // 現在値が未入力での演算子設定時はゼロとする
+                _previousInput = "0";
+            }
+            else
+            {
+                if (isComputeJustEnterd)
+                {
+                    _previousInput = _lastResult;
+                }
+                else
+                {
+                    _previousInput = _currentInput;
+                }
+            }
+
+            _operator = inputOperator;
+            _lastInput = inputOperator;
         }
     }
 }
--- WpfCalculator2025Tests/Model/CalculatorContextTests.cs
+++ WpfCalculator2025Tests/Model/CalculatorContextTests.cs
@@ -18,7 +18,229 @@ namespace WpfCalculator2025.Model.Tests
         {
             var _context = new CalculatorContext();
 
-            Assert.AreEqual(_context.DisplayText , "0");
+            Assert.AreEqual(_context.DisplayText, "0");
+        }
+
+        [TestMethod()]
+        public void 桁追加を初回行うと追加した桁の表示文字列になること()
+        {
+            var _context = new CalculatorContext();
+
+            _context.AddDigit("1");
+
+            Assert.AreEqual(_context.DisplayText, "1");
+        }
+
+        [TestMethod()]
+        public void 桁追加を行うと追加した文字列が末尾に追加された表示文字列になること()
+        {
+            var _context = new CalculatorContext();
+
+            _context.AddDigit("1");
+            _context.AddDigit("2");
+            _context.AddDigit("3");
+
+            Assert.AreEqual(_context.DisplayText, "123");
+        }
+
+        [TestMethod()]
+        public void 全クリアを行うと表示文字列がゼロになること()
+        {
+            var _context = new CalculatorContext();
+            _context.AddDigit("1");
+            _context.AddDigit("2");
+
+            _context.ClearAll();
+
+            Assert.AreEqual(_context.DisplayText, "0");
+        }
+
+        [TestMethod()]
+        public void 全クリアを行うと前入力値が空になること()
+        {
+            var _context = new CalculatorContext();
+            _context.AddDigit("1");
+            _context.AddDigit("2");
+
+            _context.SetOperator("+");
+
+            _context.ClearAll();
+
+            Assert.AreEqual(_context.PreviousInput, "");
+        }
+
+        [TestMethod()]
+        public void オペレターは初期未入力であること()
+        {
+            var _context = new CalculatorContext();
+
+            Assert.AreEqual(_context.Operator, "");
+        }
+
+        [TestMethod()]
+        public void オペレターは常に最後の入力値であること()
+        {
+            var _context = new CalculatorContext();
+
+            _context.SetOperator("+");
+
+            Assert.AreEqual(_context.Operator, "+");
+
+            _context.SetOperator("-");
+
+            Assert.AreEqual(_context.Operator, "-");
+        }
+
+        [TestMethod()]
+        public void オペレターは全クリアで未入力になること()
+        {
+            var _context = new CalculatorContext();
+
+            _context.SetOperator("+");
+
+            _context.ClearAll();
+
+            Assert.AreEqual(_context.Operator, "");
+        }
+
+        [TestMethod()]
+        public void オペレータを入力すると現在入力値がある場合は現在入力値が前入力値にセットされること()
+        {
+            var _context = new CalculatorContext();
+
+            _context.AddDigit("1");
+            _context.AddDigit("2");
+
+            _context.SetOperator("+");
+
+            Assert.AreEqual(_context.PreviousInput, "12");
+        }
+
+
+        [TestMethod()]
+        public void オペレータを入力すると現在入力値が無い場合は前入力値にゼロがセットされること()
+        {
+            var _context = new CalculatorContext();
+
+            _context.SetOperator("+");
+
+            Assert.AreEqual(_context.PreviousInput, "0");
+        }
+
+        [TestMethod()]
+        public void オペレータを入力しても表示文字列は変わらないこと()
+        {
+            var _context = new CalculatorContext();
+
+            _context.AddDigit("1");
+            _context.AddDigit("1");
+            _context.SetOperator("+");
+
+            Assert.AreEqual(_context.DisplayText, "11");
+        }
+
+        [TestMethod()]
+        public void オペレータを連続入力しても表示は変わらないこと()
+        {
+            var _context = new CalculatorContext();
+
+            _context.AddDigit("1");
+            _context.AddDigit("1");
+            _context.SetOperator("+");
+
+            _context.SetOperator("+");
+
+            _context.SetOperator("+");
+
+            Assert.AreEqual(_context.DisplayText, "11");
+        }
+
+        [TestMethod()]
+        public void 計算可能状態でオペレーターを入力すると計算結果が表示されること()
+        {
+            var _context = new CalculatorContext();
+
+            _context.AddDigit("1");
+            _context.AddDigit("1");
+            _context.SetOperator("+");
+
+            _context.AddDigit("2");
+            _context.SetOperator("+");
+
+            // 13
+
+            _context.SetOperator("+");
+
+            // 13変わらず
+            Assert.AreEqual(_context.DisplayText, "13");
+        }
+
+        [TestMethod()]
+        public void オペレータを入力した後に桁追加すると表示文字列は入力値に変わること()
+        {
+            var _context = new CalculatorContext();
+
+            _context.AddDigit("1");
+            _context.AddDigit("1");
+            _context.SetOperator("+");
+
+            _context.AddDigit("2");
+
+            Assert.AreEqual(_context.DisplayText, "2");
+        }
+
+        [TestMethod()]
+        public void 演算実行で1回加算できること()
+        {
+            var _context = new CalculatorContext();
+
+            _context.AddDigit("1");
+            _context.SetOperator("+");
+            _context.AddDigit("2");
+            _context.Compute();
+
+            Assert.AreEqual(_context.DisplayText, "3");
+        }
+
+        [TestMethod()]
+        public void 演算実行結果に演算子を入力してさらに演算実行できること()
+        {
+            var _context = new CalculatorContext();
+            _context.AddDigit("1");
+            _context.SetOperator("+");
+            _context.AddDigit("2");
+            _context.Compute();
+
+            // 3 +
+            _context.SetOperator("+");
+
+            // 3 + 5
+            _context.AddDigit("5");
+            _context.Compute();
+
+            Assert.AreEqual(_context.DisplayText, "8");
+        }
+
+        [TestMethod()]
+        public void 演算実行で連続演算を行うことができること()
+        {
+            var _context = new CalculatorContext();
+            _context.AddDigit("1");
+            _context.SetOperator("+");
+
+            // 変わらない
+            _context.AddDigit("2");
+
+            // 3
+            _context.Compute();
+
+            // 3+2
+            _context.Compute();
+
+            // 5+2
+            _context.Compute();
+
+            Assert.AreEqual(_context.DisplayText, "7");
         }
     }
 }
