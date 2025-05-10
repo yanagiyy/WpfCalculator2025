ae8179faee5fdce71b5eeea3804c1b52a3192340
yanagiyy <210198714+yanagiyy@users.noreply.github.com>
2025-05-10 14:12:54 +0900

    テスト用プロジェクトを追加

---
+++ WpfCalculator2025Tests/Model/CalculatorContextTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WpfCalculator2025.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WpfCalculator2025.Model.Tests
+{
+    [TestClass()]
+    public class CalculatorContextTests
+    {
+        [TestMethod()]
+        [TestCategory("CalculatorContext.DisplayText")]
+        public void 表示文字列は初期表示がゼロであること()
+        {
+            var _context = new CalculatorContext();
+
+            Assert.AreEqual(_context.DisplayText , "0");
+        }
+    }
+}
